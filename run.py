c282623f1484c66bc140287f5e85260e5c67be33
michel-espinoza <michel.dev26@gmail.com>
2025-11-16 21:38:14 -0300

    fs

--- run.py
+++ run.py
@@ -4,4 +4,7 @@ from app import create_app
 if __name__ == '__main__':
     config_name = os.getenv('FLASK_ENV', 'development')
     app = create_app(config_name)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    
+    # Toma el puerto de la variable PORT (Render lo define), usa 5000 solo si no está
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
